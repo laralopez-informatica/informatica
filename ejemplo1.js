324ba911014725e76dd5ed4802d0f0ff7e4f167b
lara lopez <lara1110@outlook.es>
2025-09-19 15:17:42 -0300

    Add files via upload

---
+++ ejemplo1.js
@@ -0,0 +1,4 @@
+let parrafo= document.getElementById("texto original");
+let boton=document.getElementById("btn");
+boton.addEventListener("click",function(){});
+parrafo.textContent="texto modificado";
