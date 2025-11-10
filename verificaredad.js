de80bda8cc7f2552294b961bdc89cb51b6ce5650
lara lopez <lara1110@outlook.es>
2025-11-10 19:34:52 -0300

    Add files via upload

---
+++ verificaredad.js
@@ -0,0 +1,10 @@
+document.getElementById("verificar").addEventListener("click", function(){
+    let edad = prompt("Ingrese su edad:");
+    edad = Number(edad);
+
+    if(edad >= 18){
+        alert("Acceso permitido");
+    } else {
+        alert("Acceso denegado");
+    }
+});
